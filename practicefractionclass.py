74aa6e2e9d5a7feed74036650939810452526b65
hgollakota <43627229+hgollakota@users.noreply.github.com>
2018-09-28 11:01:15 -0500

    Add files via upload

---
+++ practicefractionclass.py
@@ -0,0 +1,13 @@
+class Fraction:
+
+    def _init_(self, top, bottom):
+        self.numerator = top
+        self.denominator = bottom
+
+    def _str_(self):
+        return str(self.numerator) + "/" + str(self.denominator)
+
+
+
+
+
